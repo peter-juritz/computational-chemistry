b681d7f8ecd611d02775d8031e93df0ade5f3fce
peter-juritz <peter.juritz@gmail.com>
2016-05-05 18:41:22 +0200

    Added support for vispy

--- visualize_molecule.py
+++ visualize_molecule.py
@@ -1,15 +1,30 @@
 from molecular_geometry import GeomReader
 from visual import sphere, curve, color
 import sys
+
+SIZE_SCALE = 0.005
 at_colours = {'c':color.green,'o':color.red,'n':color.blue,'h':color.white,'s':color.black,'fe':color.red,'p':color.blue,'he':color.blue,'f':color.yellow}
 vw_radius = {'c':170,'h':120,'o':152,'n':155,'s':180,'fe':240,'p':180,'he':140,'f':147}
 def draw_atoms(mol):
     for at in mol.atoms:
     #    if at.name =='h':
             #continue
-        k = sphere(pos = at.xyz,radius = 0.005*(vw_radius[at.name]),color=at_colours[at.name])
-        at.vis = k
-        at.vis.color = k.color
+        atom_sphere = sphere(pos = at.xyz,radius = SIZE_SCALE*(vw_radius[at.name]),color=at_colours[at.name])
+        at.vis = atom_sphere
+
+def draw_atoms_vispy(mol):
+    from vispy import scene
+    from vispy.visuals.transforms import STTransform
+    canvas = scene.SceneCanvas(keys='interactive', bgcolor='black',
+                            size=(800, 600), show=True)
+    view = canvas.central_widget.add_view()
+    view.camera = 'arcball'
+    for at in mol.atoms:
+        atom_sphere = scene.visuals.Sphere(radius=SIZE_SCALE*(vw_radius[at.name]), method='ico', parent=view.scene,
+                                    color=at_colours[at.name], edge_color='black',subdivisions=2)
+
+        atom_sphere.transform = STTransform(translate=at.xyz)
+    canvas.app.run()
 
 def draw_bonds(mol):
     for l in mol.bonds:
@@ -34,8 +49,19 @@ def draw_spatial_center(mol):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print 'Usage: %s [geometry_file]'
+    if len(sys.argv) not in [2, 3]:
+        print 'Usage: %s [method] (geometry_file)' % sys.argv[0]
+        print ' Optionally specify a rendering method. Options: vpython, vispy. vpython is default'
         sys.exit(0)
-    geom = GeomReader(sys.argv[1])
-    draw_atoms(geom.mol)
+    if len(sys.argv) == 2:
+        geom = GeomReader(sys.argv[1])
+        draw_atoms(geom.mol)
+    if len(sys.argv) == 3:
+        geom = GeomReader(sys.argv[2])
+        if sys.argv[1] == 'vpython':
+            draw_atoms(geom.mol)
+        elif sys.argv[1] == 'vispy':
+            draw_atoms_vispy(geom.mol)
+        else:
+            print 'Unsupported method: %s' % sys.argv[1]
+
